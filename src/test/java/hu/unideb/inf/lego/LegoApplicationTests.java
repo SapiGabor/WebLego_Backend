76704eea64b29605ac13c91787c9ea363a4d9dd5
SapiGabor <gaborsapi9999@gmail.com>
2021-12-11 10:20:21 +0100

    First commit.

---
+++ src/test/java/hu/unideb/inf/lego/LegoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.lego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
